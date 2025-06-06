1def0116ebe28f6ccd593e5d53b39e11f8f11840
Yassin El Idrissi <yassinelidrissi1995@gmail.com>
2025-06-06 10:57:50 +0100

    Small fix

--- src/interfaces/ISwapper.sol
+++ src/interfaces/ISwapper.sol
@@ -4,5 +4,5 @@ pragma solidity ^0.8.13;
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 interface ISwapper {
-    function sell(IERC20 input, IERC20 output, uint256 amountIn, bytes calldata data) external;
+    function sell(IERC20 input, IERC20 output, uint256 amountIn) external;
 } 
