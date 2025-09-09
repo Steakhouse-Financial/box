7dc32f1d45d064e0d71be58ad08aead71a23cd3c
Yassin El Idrissi <yassinelidrissi1995@gmail.com>
2025-09-09 11:59:42 -0400

    Format and lint

--- src/lib/BoxLib.sol
+++ src/lib/BoxLib.sol
@@ -4,28 +4,19 @@ pragma solidity ^0.8.0;
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {Box} from "src/Box.sol";
 import {IBox} from "src/interfaces/IBox.sol";
-import {IOracle} from "src/interfaces/IOracle.sol";
 import {IFunding} from "src/interfaces/IFunding.sol";
-
+import {IOracle} from "src/interfaces/IOracle.sol";
 
 /// @dev Instant functions for Box governance actions, assuming 0-day timelocks
 library BoxLib {
     function addFeederInstant(Box box, address feeder) internal {
-        bytes memory encoding = abi.encodeWithSelector(
-            box.setIsFeeder.selector,
-            feeder,
-            true
-        );
+        bytes memory encoding = abi.encodeWithSelector(box.setIsFeeder.selector, feeder, true);
         box.submit(encoding);
         box.setIsFeeder(address(feeder), true);
     }
 
     function addTokenInstant(Box box, IERC20 token, IOracle oracle) internal {
-        bytes memory encoding = abi.encodeWithSelector(
-            box.addToken.selector,
-            address(token),
-            address(oracle)
-        );
+        bytes memory encoding = abi.encodeWithSelector(box.addToken.selector, address(token), address(oracle));
         box.submit(encoding);
         box.addToken(token, oracle);
     }
@@ -37,20 +28,13 @@ library BoxLib {
     }
 
     function addFundingInstant(Box box, IFunding fundingModule) internal {
-        bytes memory encoding = abi.encodeWithSelector(
-            IBox.addFunding.selector,
-            address(fundingModule)
-        );
+        bytes memory encoding = abi.encodeWithSelector(IBox.addFunding.selector, address(fundingModule));
         box.submit(encoding);
         box.addFunding(fundingModule);
     }
 
     function addFundingFacilityInstant(Box box, IFunding fundingModule, bytes memory facilityData) internal {
-        bytes memory encoding = abi.encodeWithSelector(
-            IBox.addFundingFacility.selector,
-            address(fundingModule),
-            facilityData
-        );
+        bytes memory encoding = abi.encodeWithSelector(IBox.addFundingFacility.selector, address(fundingModule), facilityData);
         box.submit(encoding);
         box.addFundingFacility(fundingModule, facilityData);
     }
@@ -66,11 +50,7 @@ library BoxLib {
     }
 
     function addFundingDebtInstant(Box box, IFunding fundingModule, IERC20 debtToken) internal {
-        bytes memory encoding = abi.encodeWithSelector(
-            IBox.addFundingDebt.selector,
-            address(fundingModule),
-            address(debtToken)
-        );
+        bytes memory encoding = abi.encodeWithSelector(IBox.addFundingDebt.selector, address(fundingModule), address(debtToken));
         box.submit(encoding);
         box.addFundingDebt(fundingModule, debtToken);
     }
